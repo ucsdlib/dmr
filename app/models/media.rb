6cb17e537dc71601513e8149b5cdebd259b9d045
VivianChu <tchu@ucsd.edu>
2015-06-29 13:34:51 -0700

    Updating the Medial model and the tests. DMR-9

--- app/models/media.rb
+++ app/models/media.rb
@@ -6,4 +6,5 @@ class Media < ActiveRecord::Base
   validates :title, presence: true 
   validates :call_number, presence: true 
   validates :year, length: { maximum: 4 }
+  validates :file_name, format: { :with => /\w(\.mp4)\z/, :message => "File name must be in .mp4 format" }
 end
--- spec/models/media_spec.rb
+++ spec/models/media_spec.rb
@@ -8,4 +8,6 @@ describe Media do
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:call_number)}
   it {should validate_length_of(:year)}
+  it {should allow_value("anyFileName.mp4").for(:file_name)}
+  it {should_not allow_value("noExtensionFilename").for(:file_name)}
 end
