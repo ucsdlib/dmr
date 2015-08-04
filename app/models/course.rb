d526ccc26f000eb1e3595d28ade5a9e834c17a7a
VivianChu <tchu@ucsd.edu>
2015-08-04 10:09:12 -0700

    Updating the regular expression. DMR-29

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,7 +6,7 @@ class Course < ActiveRecord::Base
   has_many :reports
   has_many :media, through: :reports
   validates :quarter, presence: true
-  validates :year, presence: true, length: { maximum: 4 }, format: { :with => /\A\d{4}/, :message => "must be in number format" }
+  validates :year, presence: true, length: { maximum: 4 }, format: { :with => /\A\d{4}\z/, :message => "must be in number format" }
   validates :course, presence: true
   validates :instructor, presence: true
 end
