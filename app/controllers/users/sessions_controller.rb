45dbc89f115dd47dca1815659a1cfede6973f0bc
VivianChu <tchu@ucsd.edu>
2015-09-17 11:22:55 -0700

    Refactor the login code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,9 +18,8 @@ class Users::SessionsController < ApplicationController
   def find_or_create_user(auth_type, origin)
     find_or_create_method = "find_or_create_for_#{auth_type.downcase}".to_sym
   	@user = User.send(find_or_create_method,request.env["omniauth.auth"])
-  	session[:student_user] = "true"
   	report = report_url(origin) if origin
-  	if Rails.configuration.shibboleth && !User.in_group?(request.env["omniauth.auth"].uid) && report == false
+  	if Rails.configuration.shibboleth && !User.in_group?(request.env["omniauth.auth"].uid) && report  == false
   	  render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
   	else
   	  create_user_session(@user) if @user
@@ -42,6 +41,7 @@ class Users::SessionsController < ApplicationController
   def create_user_session(user)
     session[:user_name] = user.name
     session[:user_id] = user.uid
+    session[:student_user] = "true"
   end
 
   def destroy_user_session
