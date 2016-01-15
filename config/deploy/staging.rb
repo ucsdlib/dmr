1920d53b820adbedbe10add330477f565e760f2d
VivianChu <tchu@ucsd.edu>
2016-01-15 09:33:19 -0800

    Update staging deploy config. DMR-73

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,4 +6,6 @@ set :rails_env, 'staging'
 if ENV['CAP_SSHKEY_STAGING']
   puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_STAGING"])}"
   set :ssh_options, { keys: File.join(ENV['HOME'], ENV['CAP_SSHKEY_STAGING']) }
+else
+  set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 end
