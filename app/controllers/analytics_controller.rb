7f96a3f0b7dda58d7f3eeb75185a94b5ceafda22
VivianChu <tchu@ucsd.edu>
2017-01-30 11:28:09 -0800

    Fixes #96 - Add Statistics support Audio reserves
    Update deploy config
    
    Update deploy config

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,9 +9,15 @@ class AnalyticsController < ApplicationController
     return unless params[:start_date] && params[:end_date]
     s_date = convert_time(params[:start_date], '00:00:00-8')
     e_date = convert_time(params[:end_date], '23:59:59-8')
-    @item_count = new_item_count(s_date, e_date)
-    @course_count = new_course_count(s_date, e_date) + clone_course_count(s_date, e_date)
+
     @record_count = new_record_count(s_date, e_date)
     @view_counter = view_count(s_date, e_date)
+    @audio_count = new_audio_count(s_date, e_date)
+    @new_audio_count = new_audio_record_count(s_date, e_date)
+    @item_count = new_item_count(s_date, e_date) - @audio_count
+    @audio_course_count = audio_course_count(s_date, e_date)
+    @total_course = new_course_count(s_date, e_date) + clone_course_count(s_date, e_date)
+    @course_count = @total_course - @audio_course_count
+    @audio_view_counter = audio_view_count(s_date, e_date)
   end
 end
