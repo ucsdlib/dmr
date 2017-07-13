cb2d1c544fa8773ab540f1e974b4f3d0bb119017
VivianChu <tchu@ucsd.edu>
2017-07-13 11:24:54 -0700

    Add auto update video/audio record direct link function (#188)
    Update deploy config and scheduler

--- config/deploy/pontos.rb
+++ config/deploy/pontos.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 set :stage, :pontos
-set :branch, 'feature/direct_link'
+set :branch, 'develop'
 server 'pontos.ucsd.edu', user: 'conan', roles: %w{app db}
 set :rails_env, 'pontos'
 
